d7b321d4058f9a5ae5521ee04c7d75d5ad02f735
Emmanuel3000 <ayeniemmanuel914@gmail.com>
2025-01-06 22:21:24 +0100

    made new changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,33 +4,32 @@ import HeroImage from '../assets/pf pic web.jpg'
 const Hero = () => {
   return (
     <div className='w-full min-h-screen bg-black text-white text-center py-16 pt-20'>
-  <div className='px-8 md:px-16 lg:px-24'>
-    <img
-      src={HeroImage}
-      alt='Profile picture of Emmanuel Ayeni'
-      className='mx-auto mb-8 w-48 h-48 rounded-full object-cover transform transition-transform duration-300 hover:scale-105'
-    />
-    <h1 className='text-2xl sm:text-4xl font-bold'>
-      I'm{" "}
-      <span className='text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-600'>
-        Emmanuel Ayeni
-      </span>
-      , a Front-End Developer
-    </h1>
-    <p className='mt-4 text-base sm:text-lg text-gray-300 px-4'>
-      I specialize in building modern and responsive Web applications and Websites.
-    </p>
-    <div className='mt-8 space-x-4'>
-      <button className='bg-gradient-to-r from-green-400 to-blue-500 text-white transform transition-transform duration-300 hover:scale-105 px-6 py-3 rounded font-medium'>
-        Contact me
-      </button>
-      <button className='bg-gradient-to-r from-pink-400 to-yellow-500 text-white transform transition-transform duration-300 hover:scale-105 px-6 py-3 rounded font-medium'>
-        My Resume
-      </button>
+      <div className='px-8 md:px-16 lg:px-24'>
+        <img
+          src={HeroImage}
+          alt='Profile picture of Emmanuel Ayeni'
+          className='mx-auto mb-8 w-48 h-48 rounded-full object-cover transform transition-transform duration-300 hover:scale-105'
+        />
+        <h1 className='text-2xl sm:text-4xl font-bold'>
+          I'm{" "}
+          <span className='text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-600'>
+            Emmanuel Ayeni
+          </span>
+          , a Front-End Developer
+        </h1>
+        <p className='mt-4 text-base sm:text-lg text-gray-300 px-4'>
+          I specialize in building modern and responsive Web applications and Websites.
+        </p>
+        <div className='mt-8 flex flex-col sm:flex-row sm:justify-center sm:space-x-4 space-y-4 sm:space-y-0'>
+          <button className='bg-gradient-to-r from-green-400 to-blue-500 text-white transform transition-transform duration-300 hover:scale-105 px-6 py-3 rounded font-medium'>
+            Contact me
+          </button>
+          <button className='bg-gradient-to-r from-pink-400 to-yellow-500 text-white transform transition-transform duration-300 hover:scale-105 px-6 py-3 rounded font-medium'>
+            My Resume
+          </button>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,26 +8,26 @@ const Navbar = () => {
   }
 
   return (
-    <nav className='top-0 left-0 w-full bg-black text-white px-8 md:px-16 lg:px-24 z-50'>
+    <nav className='relative w-full bg-black text-white px-8 md:px-16 lg:px-24 z-50'>
       <div className='w-full mx-auto py-2 flex justify-between items-center'>
         <div className='text-2xl font-bold'>EawebCraft</div>
-        <div className='hidden md:flex space-x-6'>
+        <div className='hidden lg:flex space-x-6'>
           <a href="#home" className='hover:text-gray-400'>Home</a>
           <a href="#about" className='hover:text-gray-400'>About Me</a>
           <a href="#services" className='hover:text-gray-400'>Services</a>
           <a href="#projects" className='hover:text-gray-400'>Projects</a>
           <a href="#contact" className='hover:text-gray-400'>Contact</a>
         </div>
-        <button className='hidden md:inline bg-gradient-to-r from-green-400 to-blue-500 text-white transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded'>
+        <button className='hidden lg:inline bg-gradient-to-r from-green-400 to-blue-500 text-white transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded'>
           Connect me
         </button>
-        <button className='md:hidden bg-black text-white p-2 rounded' onClick={toggleMenu}>
+        <button className='lg:hidden bg-black text-white p-2 rounded' onClick={toggleMenu}>
           <svg className='w-6 h-6' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
             <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M4 6h16M4 12h16m-7 6h7'></path>
           </svg>
         </button>
       </div>
-      <div className={`md:hidden fixed top-0 left-0 h-full w-full bg-black transition-transform transform ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}>
+      <div className={`lg:hidden fixed top-0 left-0 h-full w-full bg-black transition-transform transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} z-50`}>
         <div className='flex flex-col items-center justify-center h-full space-y-6'>
           <button className='absolute top-4 right-4 bg-black text-white' onClick={toggleMenu}>
             <svg className='w-6 h-6' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
